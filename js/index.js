582480acdf6689362a90849452b39ef796fe867f
Ricardo Junior <ricardodev10@yahoo.com>
2022-09-29 15:40:14 -0300

    start refactoring

--- js/index.js
+++ js/index.js
@@ -1,190 +1,197 @@
-/*
-import Sounds from "./sounds.js"
-const sound = Sounds()
-*/
-
-// controls ==============================================
-const buttonPlay = document.querySelector(".play")
-const buttonPause = document.querySelector(".pause")
-const buttonStop = document.querySelector(".stop")
-const buttonMore = document.querySelector(".more")
-const buttonLess = document.querySelector(".less")
-let timerTimeout;
-
-const minutesDisplay = document.querySelector(".minutes")
-const secondsDisplay = document.querySelector(".seconds")
-let minutes = Number(minutesDisplay.textContent)
-
-// controls functions ====================================
-function countdown() {
-  timerTimeout = setTimeout(() => {
-    let seconds = Number(secondsDisplay.textContent)
-    let minutes = Number(minutesDisplay.textContent)
-
-    updateTimerDisplay(minutes, 0)
-
-    if (minutes <= 0) {
-      timeEnd()
-      return
-    }
-
-    if (seconds <= 0) {
-      seconds = 60
-      --minutes
-    }
-
-    updateTimerDisplay(minutes, String(seconds - 1))
-
-    countdown()
-  }, 1000)
-}
-
-function toggleButton() {
-  buttonPlay.classList.toggle('hide')
-  buttonPause.classList.toggle('hide')
-}
-
-function addFiveMinutes() {
-  if (Number(minutesDisplay.textContent) <= 55) {
-    updateTimerDisplay(Number(minutesDisplay.textContent) + 5, 0)
-  }
-
-  pressButton()
-}
-
-function removeFiveMinutes() {
-  if (Number(minutesDisplay.textContent) >= 10) {
-    updateTimerDisplay(Number(minutesDisplay.textContent) - 5, 0)
-  }
-
-  pressButton()
-}
-
-function updateTimerDisplay(minutes, seconds) {
-  minutesDisplay.textContent = String(minutes).padStart(2, "0")
-  secondsDisplay.textContent = String(seconds).padStart(2, "0")
-}
-
-function resetTimer() {
-  updateTimerDisplay(minutes, 0)
-  clearTimeout(timerTimeout)
-}
-
-// controls events =======================================
-buttonMore.addEventListener('click', addFiveMinutes)
-buttonLess.addEventListener('click', removeFiveMinutes)
-
-buttonPlay.addEventListener('click', () => {
-  toggleButton()
-  countdown()
-  pressButton()
-})
-
-buttonPause.addEventListener('click', () => {
-  toggleButton()
-  clearTimeout(timerTimeout)
-  pressButton()
-})
-
-buttonStop.addEventListener('click', () => {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  resetTimer()
-  pressButton()
-})
 
 
-// sounds ================================================
-const buttonForest = document.querySelector(".forest")
-const buttonRain = document.querySelector(".rain")
-const buttonCoffeeShop = document.querySelector(".coffee-shop")
-const buttonFireplace = document.querySelector(".fireplace")
 
-const buttonPressAudio = new Audio("./sounds/Button-press.wav")
-const kitchenTimer = new Audio("./sounds/Kitchen-timer.mp3")
 
-const audioForest = new Audio("./sounds/Floresta.mp3")
-const audioRain = new Audio("./sounds/Chuva.mp3")
-const audioCoffeeShop = new Audio("./sounds/Cafeteria.mp3")
-const audioFireplace = new Audio("./sounds/Lareira.mp3")
 
-// sounds functions ======================================
-let isPlaying = false
 
-function pressButton() {
-  buttonPressAudio.play()
-}
 
-function timeEnd() {
-  kitchenTimer.play()
-}
 
-function playAudioForest() {
-  audioForest.loop = true
 
-  isPlaying ? audioForest.pause() : audioForest.play()
 
-  audioForest.onplaying = () => {
-    isPlaying = true
-    buttonForest.style.background = "var(--bg-card-2)"
-  };
+// // controls ==============================================
+// const buttonPlay = document.querySelector(".play")
+// const buttonPause = document.querySelector(".pause")
+// const buttonStop = document.querySelector(".stop")
+// const buttonMore = document.querySelector(".more")
+// const buttonLess = document.querySelector(".less")
+// let timerTimeout;
 
-  audioForest.onpause = () => {
-    isPlaying = false
-    buttonForest.style.background = "var(--bg-card-1)"
-  };
-}
+// const minutesDisplay = document.querySelector(".minutes")
+// const secondsDisplay = document.querySelector(".seconds")
+// let minutes = Number(minutesDisplay.textContent)
 
-function playAudioRain() {
-  audioRain.loop = true
+// // controls functions ====================================
+// function countdown() {
+//   timerTimeout = setTimeout(() => {
+//     let seconds = Number(secondsDisplay.textContent)
+//     let minutes = Number(minutesDisplay.textContent)
 
-  isPlaying ? audioRain.pause() : audioRain.play()
+//     updateTimerDisplay(minutes, 0)
 
-  audioRain.onplaying = () => {
-    isPlaying = true
-    buttonRain.style.background = "var(--bg-card-2)"
-  };
+//     if (minutes <= 0) {
+//       timeEnd()
+//       return
+//     }
 
-  audioRain.onpause = () => {
-    isPlaying = false
-    buttonRain.style.background = "var(--bg-card-1)"
-  };
-}
+//     if (seconds <= 0) {
+//       seconds = 60
+//       --minutes
+//     }
 
-function playAudioCoffeeShop() {
-  audioCoffeeShop.loop = true
+//     updateTimerDisplay(minutes, String(seconds - 1))
 
-  isPlaying ? audioCoffeeShop.pause() : audioCoffeeShop.play()
+//     countdown()
+//   }, 1000)
+// }
 
-  audioCoffeeShop.onplaying = () => {
-    isPlaying = true
-    buttonCoffeeShop.style.background = "var(--bg-card-2)"
-  };
+// function toggleButton() {
+//   buttonPlay.classList.toggle('hide')
+//   buttonPause.classList.toggle('hide')
+// }
 
-  audioCoffeeShop.onpause = () => {
-    isPlaying = false
-    buttonCoffeeShop.style.background = "var(--bg-card-1)"
-  };
-}
+// function addFiveMinutes() {
+//   if (Number(minutesDisplay.textContent) <= 55) {
+//     updateTimerDisplay(Number(minutesDisplay.textContent) + 5, 0)
+//   }
 
-function playAudioFireplace() {
-  audioFireplace.loop = true
+//   pressButton()
+// }
 
-  isPlaying ? audioFireplace.pause() : audioFireplace.play()
+// function removeFiveMinutes() {
+//   if (Number(minutesDisplay.textContent) >= 10) {
+//     updateTimerDisplay(Number(minutesDisplay.textContent) - 5, 0)
+//   }
 
-  audioFireplace.onplaying = () => {
-    isPlaying = true
-    buttonFireplace.style.background = "var(--bg-card-2)"
-  };
+//   pressButton()
+// }
 
-  audioFireplace.onpause = () => {
-    isPlaying = false
-    buttonFireplace.style.background = "var(--bg-card-1)"
-  };
-}
+// function updateTimerDisplay(minutes, seconds) {
+//   minutesDisplay.textContent = String(minutes).padStart(2, "0")
+//   secondsDisplay.textContent = String(seconds).padStart(2, "0")
+// }
 
-// sounds events =========================================
-buttonForest.addEventListener('click', playAudioForest)
-buttonRain.addEventListener('click', playAudioRain)
-buttonCoffeeShop.addEventListener('click', playAudioCoffeeShop)
-buttonFireplace.addEventListener('click', playAudioFireplace)
+// function resetTimer() {
+//   updateTimerDisplay(minutes, 0)
+//   clearTimeout(timerTimeout)
+// }
+
+// // controls events =======================================
+// buttonMore.addEventListener('click', addFiveMinutes)
+// buttonLess.addEventListener('click', removeFiveMinutes)
+
+// buttonPlay.addEventListener('click', () => {
+//   toggleButton()
+//   countdown()
+//   pressButton()
+// })
+
+// buttonPause.addEventListener('click', () => {
+//   toggleButton()
+//   clearTimeout(timerTimeout)
+//   pressButton()
+// })
+
+// buttonStop.addEventListener('click', () => {
+//   buttonPlay.classList.remove('hide')
+//   buttonPause.classList.add('hide')
+//   resetTimer()
+//   pressButton()
+// })
+
+
+// // sounds ================================================
+// const buttonForest = document.querySelector(".button-forest")
+// const buttonRain = document.querySelector(".button-rain")
+// const buttonCoffeeShop = document.querySelector(".button-coffee-shop")
+// const buttonFireplace = document.querySelector(".button-fireplace")
+
+// const buttonPressAudio = new Audio("./sounds/Button-press.wav")
+// const kitchenTimerAudio = new Audio("./sounds/Kitchen-timer.mp3")
+
+// const audioForest = new Audio("./sounds/Floresta.mp3")
+// const audioRain = new Audio("./sounds/Chuva.mp3")
+// const audioCoffeeShop = new Audio("./sounds/Cafeteria.mp3")
+// const audioFireplace = new Audio("./sounds/Lareira.mp3")
+
+// // sounds functions ======================================
+// let isPlaying = false
+
+// function pressButton() {
+//   buttonPressAudio.volume = 0.02
+//   buttonPressAudio.play()
+// }
+
+// function timeEnd() {
+//   kitchenTimerAudio.volume = 0.05
+//   kitchenTimerAudio.play()
+// }
+
+// function playAudioForest() {
+//   audioForest.loop = true
+
+//   isPlaying ? audioForest.pause() : audioForest.play()
+
+//   audioForest.onplaying = () => {
+//     isPlaying = true
+//     buttonForest.style.background = "var(--bg-card-2)"
+//   };
+
+//   audioForest.onpause = () => {
+//     isPlaying = false
+//     buttonForest.style.background = "var(--bg-card-1)"
+//   };
+// }
+
+// function playAudioRain() {
+//   audioRain.loop = true
+
+//   isPlaying ? audioRain.pause() : audioRain.play()
+
+//   audioRain.onplaying = () => {
+//     isPlaying = true
+//     buttonRain.style.background = "var(--bg-card-2)"
+//   };
+
+//   audioRain.onpause = () => {
+//     isPlaying = false
+//     buttonRain.style.background = "var(--bg-card-1)"
+//   };
+// }
+
+// function playAudioCoffeeShop() {
+//   audioCoffeeShop.loop = true
+
+//   isPlaying ? audioCoffeeShop.pause() : audioCoffeeShop.play()
+
+//   audioCoffeeShop.onplaying = () => {
+//     isPlaying = true
+//     buttonCoffeeShop.style.background = "var(--bg-card-2)"
+//   };
+
+//   audioCoffeeShop.onpause = () => {
+//     isPlaying = false
+//     buttonCoffeeShop.style.background = "var(--bg-card-1)"
+//   };
+// }
+
+// function playAudioFireplace() {
+//   audioFireplace.loop = true
+
+//   isPlaying ? audioFireplace.pause() : audioFireplace.play()
+
+//   audioFireplace.onplaying = () => {
+//     isPlaying = true
+//     buttonFireplace.style.background = "var(--bg-card-2)"
+//   };
+
+//   audioFireplace.onpause = () => {
+//     isPlaying = false
+//     buttonFireplace.style.background = "var(--bg-card-1)"
+//   };
+// }
+
+// // sounds events =========================================
+// buttonForest.addEventListener('click', playAudioForest)
+// buttonRain.addEventListener('click', playAudioRain)
+// buttonCoffeeShop.addEventListener('click', playAudioCoffeeShop)
+// buttonFireplace.addEventListener('click', playAudioFireplace)
